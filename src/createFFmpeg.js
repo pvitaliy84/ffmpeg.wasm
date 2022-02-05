1d8695348439b0df844fb54e82e6a44c7d81d40d
Vitaliy Pozdnyakov <pvitaliy84@mail.ru>
2022-02-05 14:25:42 +0600

    ...

--- src/createFFmpeg.js
+++ src/createFFmpeg.js
@@ -83,7 +83,7 @@ module.exports = (_options = {}) => {
           return prefix + path;
         },
       });
-      ffmpeg = Core.cwrap('_main', 'number', ['number', 'number']);
+      ffmpeg = Core.cwrap('main', 'number', ['number', 'number']);
       log('info', 'ffmpeg-core loaded');
     } else {
       throw Error('ffmpeg.wasm was loaded, you should not load it again, use ffmpeg.isLoaded() to check next time.');
